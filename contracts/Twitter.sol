905649903b484fa395296a7f953e675679dbb4fc
Ceejay Santos <7078ceejay@gmail.com>
2025-09-15 19:27:08 +0800

    adding structs in the contract

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -3,22 +3,35 @@ pragma solidity ^0.8.28;
 
 contract Twitter{
 
-    
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
 
-    mapping(address => string[]) public tweets;
+    mapping(address => Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public{
-        return tweets[msg.sender].push(_tweet);
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+
+       tweets[msg.sender].push(newTweet);
     }
 
     // memory = temporary storage
     // view = no data changes when the function runs
 
-    function getTweet(address _owner, uint _i) public view returns (string memory){
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory){
         return tweets[_owner][_i];
     }
 
-    function getAllTweets(address _owner) public view returns (string[] memory){
+    function getAllTweets(address _owner) public view returns (Tweet[] memory){
         return tweets[_owner];
     }
 
